b4262e9a5f0b9e32654360db327deedab5b81dac
thetlin-dev <thetlinzaw72@gmail.com>
2026-09-10 15:00:44 +0630

    Refactor media picking and update gallery button

--- lib/create_post_screen.dart
+++ lib/create_post_screen.dart
@@ -86,7 +86,7 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
     );
   }
 
- Future<void> _pickMedia([bool isVideo = false]) async {
+Future<void> _pickMedia([bool isVideo = false]) async {
   final XFile? file = isVideo
       ? await _picker.pickVideo(source: ImageSource.gallery)
       : await _picker.pickImage(source: ImageSource.gallery);
@@ -97,7 +97,8 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
       _isVideo = isVideo;
     });
   }
-} 
+}
+
 
   Future<void> _handleShutterPress() async {
     if (_selectedFile != null) {
@@ -290,20 +291,20 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        // Gallery Button
-GestureDetector(
-  onTap: () => _pickMedia(false), // ဓာတ်ပုံရွေးရန်အတွက် false တိုက်ရိုက်ပေးပါ
-  child: Container(
-    width: 44,
-    height: 44,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(8),
-      border: Border.all(color: Colors.white, width: 2),
-      color: Colors.grey[850],
-    ),
-    child: const Icon(Icons.photo_library, color: Colors.white, size: 24),
-  ),
-),
+        // Gallery Button
+        GestureDetector(
+          onTap: () => _pickMedia(false),
+          child: Container(
+            width: 44,
+            height: 44,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Colors.white, width: 2),
+              color: Colors.grey[850],
+            ),
+            child: const Icon(Icons.photo_library, color: Colors.white, size: 24),
+          ),
+        ),
 
                         // Shutter Button (Click to Snap or Record)
                         GestureDetector(
